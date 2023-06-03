bfbb169646bc169feedaeca2ffd2bdde5cec3337
Koki Sakano <kosakanoki@gmail.com>
2023-06-03 14:58:05 +0900

    追加したルールを反映

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react';
 
 export const Button: FC = () => {
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
       Button
     </button>
   );
