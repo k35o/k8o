cbec60b990e38610ad8ae123f7267dd97be322b0
Koki Sakano <kosakanoki@gmail.com>
2023-06-05 18:06:01 +0900

    roundedをつける

--- src/app/characters/components/app-card/app-card.tsx
+++ src/app/characters/components/app-card/app-card.tsx
@@ -13,7 +13,7 @@ export const AppCard = <T extends string>({
   description: string;
 }) => {
   return (
-    <section className="h-40 border-2 border-neutral-500">
+    <section className="h-40 rounded-md border-2 border-neutral-500">
       <Link href={link}>
         <div className="flex gap-6 p-4">
           <div className="flex h-32 w-32 shrink-0 items-center justify-center bg-neutral-900 text-7xl">
