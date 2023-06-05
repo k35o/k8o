70dd9926b432a985c2b19befd2064e6561c820ac
Koki Sakano <kosakanoki@gmail.com>
2023-06-05 18:31:51 +0900

    文字数を数える状態作成

--- src/app/characters/counter/state/text.ts
+++ src/app/characters/counter/state/text.ts
@@ -1,10 +1,21 @@
 import { RECOIL_KEYS } from '@/constants';
 import { ChangeEvent } from 'react';
-import { atom, useRecoilState } from 'recoil';
+import { atom, selector, useRecoilState } from 'recoil';
 
 export const textState = atom<string>({
   key: RECOIL_KEYS.CHARACTERS_COUNTER_TEXT,
   default: '',
+});
+
+export const textLengthState = selector<number>({
+  key: RECOIL_KEYS.CHARACTERS_COUNTER_TEXT_LENGTH,
+  get: ({ get }) => {
+    const text = get(textState);
+    const segmenter = new Intl.Segmenter('ja', {
+      granularity: 'grapheme',
+    });
+    return [...segmenter.segment(text)].length;
+  },
 });
 
 export const useTextField = () => {
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const RECOIL_KEYS = {
   CHARACTERS_COUNTER_TEXT: 'characters.counter.text',
+  CHARACTERS_COUNTER_TEXT_LENGTH: 'characters.counter.text.length',
 };
