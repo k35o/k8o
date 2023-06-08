4b6cdef34277882c4afc21917505f2f562614545
Koki Sakano <kosakanoki@gmail.com>
2023-06-08 14:46:10 +0900

    幅調整

--- src/components/global-layout/global-layout.tsx
+++ src/components/global-layout/global-layout.tsx
@@ -8,14 +8,14 @@ export const GlobalLayout: FC<{ children: ReactNode }> = ({
   return (
     <div className="flex min-h-screen flex-col">
       <header className="flex items-center justify-center p-4">
-        <div className="w-full max-w-5xl">
+        <div className="w-full max-w-4xl">
           <Link href="/">
             <h1 className="text-3xl font-bold">k8o</h1>
           </Link>
         </div>
       </header>
       <main className="flex grow justify-center bg-slate-300">
-        <div className="w-full max-w-5xl px-4 py-10">{children}</div>
+        <div className="w-full max-w-4xl px-4 py-10">{children}</div>
       </main>
     </div>
   );
