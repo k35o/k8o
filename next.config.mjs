24515de91cfb32a4fbec69dad24b8e37a46fdd29
Koki Sakano <kosakanoki@gmail.com>
2023-06-22 18:51:05 +0900

    toolの導入

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+import withMdx from "@next/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
+import { getHighlighter } from "shiki";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+  },
+};
+
+export default withMdx({
+  options: {
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        /** @type {Partial<import("rehype-pretty-code").Options>} */
+        ({
+          theme: { dark: "one-dark-pro" },
+          getHighlighter,
+        }),
+      ],
+    ],
+  },
+})(nextConfig);
