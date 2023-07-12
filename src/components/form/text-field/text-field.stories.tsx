252e4373bc63843d88316b33666ad3412bea522b
Koki Sakano <kosakanoki@gmail.com>
2023-07-12 11:17:36 +0900

    TextFieldの作成

---
+++ src/components/form/text-field/text-field.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { TextField } from './text-field';
+
+const meta: Meta<typeof TextField> = {
+  title: 'form/text-field',
+  component: TextField,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof TextField>;
+
+export const Primary: Story = {
+  args: {
+    value: '10',
+    onChange: (value: string) => console.log(value),
+  },
+};
