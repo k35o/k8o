0d800d418362902bd55ab33d1b10442b30f6deb4
Koki Sakano <kosakanoki@gmail.com>
2023-07-12 11:16:32 +0900

    TextAreaの切り出し

--- src/app/characters/counter/components/text-field/text-field.tsx
+++ src/app/characters/counter/components/text-field/text-field.tsx
@@ -1,15 +1,15 @@
 'use client';
 
+import { Textarea } from '@/components/form/textarea';
 import { useTextField } from '../../state/text';
 
 export const TextField = () => {
   const { text, handleTextChange } = useTextField();
 
   return (
-    <textarea
+    <Textarea
       value={text}
       onChange={handleTextChange}
-      className="w-full rounded-md border border-gray-300 p-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
       placeholder="ここに文字列を入力してください"
       rows={5}
     />
