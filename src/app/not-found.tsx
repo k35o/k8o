23b17632d17ab5f9f626cd1e25e836282c3b26e8
kosakanoki <61353435+KokiSakano@users.noreply.github.com>
2023-07-12 23:14:32 +0900

    not-found作成

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center">
+      <p className="text-cran-400 text-9xl font-bold">404</p>
+      <p className="text-xl font-bold">This page could not be found.</p>
+    </div>
+  );
+}
