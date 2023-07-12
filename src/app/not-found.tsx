943d86ed934ba8f1903672e95dd057128e73ef51
Koki Sakano <kosakanoki@gmail.com>
2023-07-13 08:37:53 +0900

    中央寄せ

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,10 @@
 export default function NotFound() {
   return (
-    <div className="flex flex-col gap-2 items-center justify-center">
-      <p className="text-cyan-400 text-9xl font-bold">404</p>
-      <p className="text-xl font-bold">This page could not be found.</p>
+    <div className="flex h-full flex-col items-center justify-center gap-2">
+      <p className="text-9xl font-bold text-cyan-400">404</p>
+      <p className="text-xl font-bold">
+        This page could not be found.
+      </p>
     </div>
   );
 }
