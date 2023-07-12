e6ca42c68373890659403426d775fb76eedca6b7
Koki Sakano <kosakanoki@gmail.com>
2023-07-12 11:17:53 +0900

    基数を変換するコンポーネントの作成

---
+++ src/app/converter/components/base-converter/base-converter.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { BaseConverter } from './base-converter';
+
+const meta: Meta<typeof BaseConverter> = {
+  title: 'app/converter/base-converter',
+  component: BaseConverter,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof BaseConverter>;
+
+export const Primary: Story = {};
