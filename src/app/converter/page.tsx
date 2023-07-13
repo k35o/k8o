ad31f0008dbffb6d6c50e06527988ca312c5f1c4
Koki Sakano <kosakanoki@gmail.com>
2023-07-13 15:41:00 +0900

    section化

--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -4,18 +4,18 @@ import { ColorConverter } from './_components/color-converter';
 export default function Page() {
   return (
     <div className="flex flex-col gap-8">
-      <div className="flex flex-col gap-4">
+      <section className="flex flex-col gap-4">
         <h3 className="text-l font-medium">進数</h3>
         <div className="rounded-md bg-white p-4 shadow-md">
           <BaseConverter />
         </div>
-      </div>
-      <div className="flex flex-col gap-4">
+      </section>
+      <section className="flex flex-col gap-4">
         <h3 className="text-l font-medium">カラーコード</h3>
         <div className="rounded-md bg-white p-4 shadow-md">
           <ColorConverter />
         </div>
-      </div>
+      </section>
     </div>
   );
 }
