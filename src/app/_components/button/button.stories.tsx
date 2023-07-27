5d59104239625956afe84acd3c1f609813db8f2d
Koki Sakano <kosakanoki@gmail.com>
2023-07-27 16:42:09 +0900

    ボタンの作成

---
+++ src/app/_components/button/button.stories.tsx
@@ -0,0 +1,63 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './button';
+
+const meta: Meta<typeof Button> = {
+  title: 'button',
+  component: Button,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const FullWidth: Story = {
+  args: {
+    type: 'button',
+    fullWidth: true,
+    onClick: () => {
+      console.log('clicked');
+    },
+  },
+  render: (props) => {
+    return <Button {...props}>ボタン</Button>;
+  },
+};
+
+export const Large: Story = {
+  args: {
+    type: 'button',
+    size: 'lg',
+    onClick: () => {
+      console.log('clicked');
+    },
+  },
+  render: (props) => {
+    return <Button {...props}>ボタン</Button>;
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    type: 'button',
+    size: 'md',
+    onClick: () => {
+      console.log('clicked');
+    },
+  },
+  render: (props) => {
+    return <Button {...props}>ボタン</Button>;
+  },
+};
+
+export const Small: Story = {
+  args: {
+    type: 'button',
+    size: 'sm',
+    onClick: () => {
+      console.log('clicked');
+    },
+  },
+  render: (props) => {
+    return <Button {...props}>ボタン</Button>;
+  },
+};
