72222b9c49f74efc44b16760fb8cd7ed94a55f35
Koki Sakano <kosakanoki@gmail.com>
2023-07-27 19:03:53 +0900

    apiの作成

---
+++ src/app/characters/check-syntax/_utils/japaneseSyntax.ts
@@ -0,0 +1,46 @@
+'use server';
+
+const JAPANESE_SYNTAX_CHECK_API =
+  'https://japanese-syntax-checker.vercel.app/api';
+
+type Request = {
+  text: string;
+};
+
+type Response = {
+  text: string;
+  msgs: {
+    type: 'lint';
+    ruleId: string;
+    message: string;
+    index: number;
+    line: number;
+    column: number;
+    range: number[];
+    loc: {
+      start: {
+        line: number;
+        column: number;
+      };
+      end: {
+        line: number;
+        column: number;
+      };
+    };
+    severity: number;
+  }[];
+};
+
+export const checkJapaneseSyntax = async (
+  request: Request,
+): Promise<Response> => {
+  const res = await fetch(JAPANESE_SYNTAX_CHECK_API, {
+    method: 'POST',
+    body: JSON.stringify(request),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const json = await res.json();
+  return json;
+};
