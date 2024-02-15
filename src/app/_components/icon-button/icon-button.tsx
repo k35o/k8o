3109c8c4b965c01e99f240c9fb97676e07574883
Koki Sakano <kosakanoki@gmail.com>
2024-02-15 18:38:45 +0900

    Icon Buttonの作成

---
+++ src/app/_components/icon-button/icon-button.tsx
@@ -0,0 +1,37 @@
+import { FC, PropsWithChildren, ReactEventHandler } from 'react';
+
+type Props = PropsWithChildren<{
+  size?: 'sm' | 'md' | 'lg';
+  disabled?: boolean;
+  label: string;
+  onClick: ReactEventHandler<HTMLButtonElement>;
+}>;
+
+export const IconButton: FC<Props> = ({
+  size = 'md',
+  disabled = false,
+  label,
+  onClick,
+  children,
+}) => {
+  return (
+    <button
+      className={`
+        flex items-center justify-center
+        rounded-full bg-white
+        hover:bg-gray-100
+        focus-visible:ring-2
+        focus-visible:ring-blue-500 active:bg-gray-200
+        ${size === 'sm' && 'p-1'}
+        ${size === 'md' && 'p-2'}
+        ${size === 'lg' && 'p-3'}
+        ${disabled && 'cursor-not-allowed opacity-50'}
+      `}
+      disabled={disabled}
+      onClick={onClick}
+      aria-label={label}
+    >
+      {children}
+    </button>
+  );
+};
