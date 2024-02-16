c2471691029b4f3efe0681f2d1a2d909faf5201b
Koki Sakano <kosakanoki@gmail.com>
2024-02-16 17:51:16 +0900

    上でキャッチさせないように

--- src/app/_components/form/textarea/textarea.tsx
+++ src/app/_components/form/textarea/textarea.tsx
@@ -31,6 +31,9 @@ export const Textarea: FC<Props> = ({
       className="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
       placeholder={placeholder}
       rows={rows}
+      onKeyDown={(e) => {
+        e.stopPropagation();
+      }}
     />
   );
 };
