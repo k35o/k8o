fb53ef06a913cbbaaf1b1901ae7a85a813e5dfde
Koki Sakano <kosakanoki@gmail.com>
2024-07-20 21:53:22 +0900

    text-areadの修正

--- src/app/characters/counter/_components/text-field/text-field.tsx
+++ src/app/characters/counter/_components/text-field/text-field.tsx
@@ -3,27 +3,32 @@
 import { Textarea } from '@/components/form/textarea';
 import { useTextField } from '../../_state/text';
 import { TextLength } from '../text-length';
-import { useDeferredValue, useId } from 'react';
+import { useDeferredValue } from 'react';
+import { FormControl } from '@/components/form/form-control/form-control';
 
 export const TextField = () => {
-  const id = useId();
   const { text, handleTextChange } = useTextField();
   const deferredText = useDeferredValue(text);
 
   return (
     <div className="flex-grow">
-      <fieldset className="flex h-full flex-col gap-2">
-        <label className="font-bold" htmlFor={id}>
-          カウントしたい文字列
-        </label>
-        <Textarea
-          id={id}
-          value={text}
-          onChange={handleTextChange}
-          placeholder="ここに文字列を入力してください"
-          fullHeight
+      <div className="h-full *:h-full">
+        <FormControl
+          label="カウントしたい文字列"
+          renderInput={(props) => {
+            return (
+              <Textarea
+                {...props}
+                value={text}
+                onChange={handleTextChange}
+                placeholder="ここに文字列を入力してください"
+                fullHeight
+                isRequired
+              />
+            );
+          }}
         />
-      </fieldset>
+      </div>
       <div className="flex gap-2">
         <p className="text-lg">文字数：</p>
         <p className="text-xl font-bold">
