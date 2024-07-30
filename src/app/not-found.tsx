60a37e905b2f172eece69c7b6c937b36e40f3188
Koki Sakano <kosakanoki@gmail.com>
2024-07-30 13:42:56 +0900

    トップへ戻るリンクの作成

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image';
 import notFoundImage from './_images/404.png';
+import { LinkButton } from '@/components/link-button';
 
 export default function NotFound() {
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <Image alt="404 Not Found" src={notFoundImage} />
+      <LinkButton href="/" size="lg">
+        トップへ戻る
+      </LinkButton>
     </div>
   );
 }
