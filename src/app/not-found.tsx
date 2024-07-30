4d725b74a1794d539e5fd4039f5a20f6e0736ad6
Koki Sakano <kosakanoki@gmail.com>
2024-07-30 11:58:34 +0900

    画像を用いて404ページに更新

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,10 @@
+import Image from 'next/image';
+import notFoundImage from './_images/404.png';
+
 export default function NotFound() {
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-2 rounded-lg bg-white">
-      <p className="text-9xl font-bold text-cyan-400">404</p>
-      <p className="text-xl font-bold">
-        This page could not be found.
-      </p>
+    <div className="flex h-full flex-col items-center justify-center">
+      <Image alt="404 Not Found" src={notFoundImage} />
     </div>
   );
 }
