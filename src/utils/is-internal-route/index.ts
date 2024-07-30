e527cbbac23d712c38306f50527d56912ee99e19
Koki Sakano <kosakanoki@gmail.com>
2024-07-30 16:43:03 +0900

    testの追加

--- src/utils/is-internal-route/index.ts
+++ src/utils/is-internal-route/index.ts
@@ -3,3 +3,13 @@ import { Route } from 'next';
 // NOTE:型推論は厳密ではないので注意が必要
 export const isInternalRoute = (href: string): href is Route =>
   !href.startsWith('http');
+
+if (import.meta.vitest) {
+  it('httpから始まるhrefはexternalなものとして判定する', () => {
+    expect(isInternalRoute('https://k8o.me/dummy')).toBe(false);
+  });
+
+  it('httpから始まらないhrefはexternalなものとして判定する', () => {
+    expect(isInternalRoute('/dummy')).toBe(true);
+  });
+}
