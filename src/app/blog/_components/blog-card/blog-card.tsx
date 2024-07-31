4e498edd91230148749cde1adbe35812c79022ad
Koki Sakano <kosakanoki@gmail.com>
2024-07-31 19:19:05 +0900

    internalRouteを使う

--- src/app/blog/_components/blog-card/blog-card.tsx
+++ src/app/blog/_components/blog-card/blog-card.tsx
@@ -1,9 +1,9 @@
 import { Heading } from '@/components/heading';
 import { TextTag } from '@/components/text-tag';
-import { Route } from 'next';
+import { isInternalRoute } from '@/utils/is-internal-route';
 import Link from 'next/link';
 
-export const BlogCard = <T extends string>({
+export const BlogCard = ({
   link,
   emotion,
   title,
@@ -14,10 +14,32 @@ export const BlogCard = <T extends string>({
   title: string;
   tags: [string?, string?, string?, string?, string?];
 }) => {
-  const isExternal = link.startsWith('http');
   return (
     <section className="h-40 rounded-xl bg-white shadow-md">
-      {isExternal ? (
+      {isInternalRoute(link) ? (
+        <Link href={link}>
+          <div className="flex gap-6 p-4">
+            <div className="flex h-32 w-32 shrink-0 items-center justify-center rounded-lg bg-bgLight text-7xl">
+              {emotion}
+            </div>
+            <div className="flex w-full flex-col justify-around">
+              <Heading type="h3" lineClamp={3}>
+                {title}
+              </Heading>
+              {tags.length > 0 && (
+                <div className="hidden justify-end gap-2 md:flex">
+                  {tags.map((tag) => {
+                    if (!tag) {
+                      return null;
+                    }
+                    return <TextTag key={tag} text={tag} />;
+                  })}
+                </div>
+              )}
+            </div>
+          </div>
+        </Link>
+      ) : (
         <a href={link} target="_blank" rel="noopener noreferrer">
           <div className="flex gap-6 p-4">
             <div className="flex h-32 w-32 shrink-0 items-center justify-center rounded-lg bg-bgLight text-7xl">
@@ -40,29 +62,6 @@ export const BlogCard = <T extends string>({
             </div>
           </div>
         </a>
-      ) : (
-        <Link href={link as Route<T>}>
-          <div className="flex gap-6 p-4">
-            <div className="flex h-32 w-32 shrink-0 items-center justify-center rounded-lg bg-bgLight text-7xl">
-              {emotion}
-            </div>
-            <div className="flex w-full flex-col justify-around">
-              <Heading type="h3" lineClamp={3}>
-                {title}
-              </Heading>
-              {tags.length > 0 && (
-                <div className="hidden justify-end gap-2 md:flex">
-                  {tags.map((tag) => {
-                    if (!tag) {
-                      return null;
-                    }
-                    return <TextTag key={tag} text={tag} />;
-                  })}
-                </div>
-              )}
-            </div>
-          </div>
-        </Link>
       )}
     </section>
   );
