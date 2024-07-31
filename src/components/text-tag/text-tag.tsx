c1fa83d58766c9aeb6b27c1d612f27f866d60616
Koki Sakano <kosakanoki@gmail.com>
2024-07-31 19:14:56 +0900

    colorの調整

--- src/components/text-tag/text-tag.tsx
+++ src/components/text-tag/text-tag.tsx
@@ -1,6 +1,6 @@
 export const TextTag = ({ text }: { text: string }) => {
   return (
-    <span className="inline-block rounded-full bg-gray px-3 py-1 text-sm font-medium">
+    <span className="inline-block rounded-full bg-bgBase px-3 py-1 text-sm font-medium">
       {text}
     </span>
   );
