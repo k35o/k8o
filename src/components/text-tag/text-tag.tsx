4c5b9df6bfdf9d6238a311468fcf8ceddcef9154
Koki Sakano <kosakanoki@gmail.com>
2024-08-04 17:44:20 +0900

    texttagを背景の色と分ける

--- src/components/text-tag/text-tag.tsx
+++ src/components/text-tag/text-tag.tsx
@@ -1,6 +1,6 @@
 export const TextTag = ({ text }: { text: string }) => {
   return (
-    <span className="inline-block rounded-full bg-bgBase px-3 py-1 text-sm font-medium">
+    <span className="inline-block rounded-full bg-bgLight px-3 py-1 text-sm font-medium">
       {text}
     </span>
   );
