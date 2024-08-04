161454a19e4eec4ad05e05c3a56d2361502e49eb
Koki Sakano <kosakanoki@gmail.com>
2024-08-04 19:54:19 +0900

    一覧を作る

--- src/app/quizzes/fish-kanji/page.tsx
+++ src/app/quizzes/fish-kanji/page.tsx
@@ -2,6 +2,7 @@ import { getQuizzes } from '@/drizzle/db';
 import { QUIZ_TYPE } from './_utils/constants';
 import { Question } from '../_components/question';
 import { FishKanjiCollection } from './_components/fish-kanji-collection';
+import { LinkButton } from '@/components/link-button';
 
 export const revalidate = 0;
 
@@ -12,9 +13,16 @@ export default async function Page() {
   });
 
   return (
-    <Question
-      quizzes={quizzes}
-      collection={<FishKanjiCollection />}
-    />
+    <div className="flex flex-col gap-2">
+      <div className="flex items-center justify-end">
+        <LinkButton href="/quizzes/fish-kanji/list">
+          うおへんの漢字一覧
+        </LinkButton>
+      </div>
+      <Question
+        quizzes={quizzes}
+        collection={<FishKanjiCollection />}
+      />
+    </div>
   );
 }
