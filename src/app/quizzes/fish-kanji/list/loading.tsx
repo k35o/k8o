90979e83957fb6f88d583cc9cc0189a6a47a82ae
Koki Sakano <kosakanoki@gmail.com>
2024-08-05 19:23:31 +0900

    loadingの調整

--- src/app/quizzes/fish-kanji/list/loading.tsx
+++ src/app/quizzes/fish-kanji/list/loading.tsx
@@ -1,7 +1,16 @@
+import { Heading } from '@/components/heading';
+import { LinkButton } from '@/components/link-button';
+
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center">
-      <p>漢字を読み込み中です...</p>
+    <div className="flex h-full flex-col items-center justify-center">
+      <div className="flex w-full items-center justify-between">
+        <Heading type="h4">漢字一覧</Heading>
+        <LinkButton href="/quizzes/fish-kanji">問題を解く</LinkButton>
+      </div>
+      <div className="flex grow items-center justify-center">
+        <p className="text-2xl font-bold">漢字を読み込み中です...</p>
+      </div>
     </div>
   );
 }
