b6e4c20750d2da730435af4023745b1116481e55
Koki Sakano <kosakanoki@gmail.com>
2024-08-24 12:14:28 +0900

    postcssの設定ファイルをts化

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,9 +1,10 @@
-/** @type {import('postcss-load-config').Config} */
+import { Config } from 'postcss-load-config';
+
 const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+} satisfies Config;
 
 export default config;
