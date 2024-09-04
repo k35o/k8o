d29eb0a6d50d2c1711758858b1244c74bf32a624
Koki Sakano <kosakanoki@gmail.com>
2024-09-04 20:35:32 +0900

    blogの順番を変える

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,16 +4,16 @@ export default function Page() {
   return (
     <div className="flex flex-col gap-4">
       <BlogCard
-        link="/blog/tanstack-router-introduction"
-        emotion="😃"
-        title="Reactの新しいルーティングライブラリ、TanStackRouterを学ぶ"
-        tags={['React', 'TypeScript', 'TanStackRouter']}
-      />
-      <BlogCard
         link="/blog/color-contrast"
         emotion="⚖️"
         title="色のコントラスト比は重要だけどどうやって求めるんだっけ？"
         tags={['color contrast', 'a11y', 'WCAG', 'W3C']}
+      />
+      <BlogCard
+        link="/blog/tanstack-router-introduction"
+        emotion="😃"
+        title="Reactの新しいルーティングライブラリ、TanStackRouterを学ぶ"
+        tags={['React', 'TypeScript', 'TanStackRouter']}
       />
     </div>
   );
