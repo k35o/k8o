d73890bfb2d66ea810ac7fdd6f07ef64c63d6dfd
Koki Sakano <kosakanoki@gmail.com>
2024-09-21 14:54:01 +0900

    修正もれ

--- src/app/blog/_components/view-counter/view-counter.stories.tsx
+++ src/app/blog/_components/view-counter/view-counter.stories.tsx
@@ -1,17 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { ViewCoounter } from './view-counter';
+import { ViewCounter } from './view-counter';
 import { kv } from '#src/mocks/vercel-kv.mock';
 
-const meta: Meta<typeof ViewCoounter> = {
+const meta: Meta<typeof ViewCounter> = {
   title: 'app/blog/view-counter',
-  component: ViewCoounter,
+  component: ViewCounter,
   beforeEach: () => {
     kv.incr.mockResolvedValue(74931);
   },
 };
 
 export default meta;
-type Story = StoryObj<typeof ViewCoounter>;
+type Story = StoryObj<typeof ViewCounter>;
 
 export const Primary: Story = {
   args: {
