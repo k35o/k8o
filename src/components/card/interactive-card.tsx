492cc9a1edb8a05d14347d7af53f7c903c35783a
Koki Sakano <kosakanoki@gmail.com>
2024-09-29 18:15:58 +0900

    interativeの時は操作可能が伝わるようにする

--- src/components/card/interactive-card.tsx
+++ src/components/card/interactive-card.tsx
@@ -10,6 +10,8 @@ export const InteractiveCard: FC<CardProps> = ({
   variant = 'primary',
 }) => (
   <motion.section
+    whileHover={{ scale: 1.05 }}
+    whileTap={{ scale: 0.95 }}
     className={clsx(
       'rounded-xl bg-bgBase/55 shadow-md',
       variant === 'primary' && 'bg-bgBase/55',
