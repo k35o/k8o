a32089078fef1a32472f6f0f8983e23b6341e0d8
Koki Sakano <kosakanoki@gmail.com>
2024-10-14 15:34:42 +0900

    app-backgroundを内側に持ってくる

--- src/app/_components/global-layout/global-layout.tsx
+++ src/app/_components/global-layout/global-layout.tsx
@@ -7,7 +7,7 @@ export const GlobalLayout: FC<{ children: ReactNode }> = ({
   children,
 }) => {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="app-background flex min-h-screen flex-col">
       <header className="flex items-center justify-center border-b border-borderPrimary p-4">
         <div className="flex w-full max-w-4xl items-center justify-between px-4">
           <Link href="/">
