9b435322ff22a04fb30b7f78257e30d33030b2ba
Koki Sakano <kosakanoki@gmail.com>
2024-10-14 20:13:24 +0900

    調整

--- test-runner.ts
+++ test-runner.ts
@@ -1,11 +1,20 @@
-import type { TestRunnerConfig } from '@storybook/test-runner';
-import { injectAxe, checkA11y } from 'axe-playwright';
+import {
+  getStoryContext,
+  type TestRunnerConfig,
+} from '@storybook/test-runner';
+import { injectAxe, checkA11y, configureAxe } from 'axe-playwright';
 
 const config: TestRunnerConfig = {
   async preVisit(page) {
     await injectAxe(page);
   },
-  async postVisit(page) {
+  async postVisit(page, context) {
+    const storyContext = await getStoryContext(page, context);
+
+    await configureAxe(page, {
+      rules: storyContext.parameters?.a11y?.config?.rules,
+    });
+
     await checkA11y(page, '#storybook-root', {
       detailedReport: true,
       detailedReportOptions: {
