5cd1156d64739cfc32d70644e81012ba4f415c3c
Koki Sakano <kosakanoki@gmail.com>
2024-10-20 16:39:20 +0900

    modalの動作を抑える

--- src/components/card/interactive-card.tsx
+++ src/components/card/interactive-card.tsx
@@ -5,13 +5,12 @@ import { FC } from 'react';
 import { CardProps } from './type';
 import clsx from 'clsx';
 
-export const InteractiveCard: FC<CardProps> = ({
-  children,
-  variant = 'primary',
-}) => (
+export const InteractiveCard: FC<
+  CardProps & { animation?: 'low' | 'medium' }
+> = ({ children, variant = 'primary', animation = 'medium' }) => (
   <motion.section
-    whileHover={{ scale: 1.05 }}
-    whileTap={{ scale: 0.95 }}
+    whileHover={{ scale: animation === 'medium' ? 1.05 : 1.02 }}
+    whileTap={{ scale: animation === 'medium' ? 0.95 : 0.98 }}
     className={clsx(
       'rounded-xl bg-bgBase/55 shadow-md',
       variant === 'primary' && 'bg-bgBase/55',
