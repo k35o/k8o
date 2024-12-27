20b7efc5c3e5d483bd00375943fcf7bbd49f44fd
Koki Sakano <kosakanoki@gmail.com>
2024-12-27 15:17:55 +0900

    eslintとtypescriptの設定を導入

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
 import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import configPrettier from "eslint-config-prettier";
 import storybook from 'eslint-plugin-storybook'
 import jsxA11y from 'eslint-plugin-jsx-a11y';
@@ -10,7 +12,10 @@ const compat = new FlatCompat({
 /**
  * @type {import('eslint').Linter.Config}
  */
-const eslintConfig = [
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
   ...compat.config({
     extends: ['next/core-web-vitals'],
   }),
@@ -25,6 +30,5 @@ const eslintConfig = [
     rules: {
       'react-hooks/rules-of-hooks': 'off',
     },
-  }
-]
-export default eslintConfig
+  },
+);
