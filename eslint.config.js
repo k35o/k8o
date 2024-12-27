db78c14f7b1a8a0e6241cc569110eaf5118f7259
Koki Sakano <kosakanoki@gmail.com>
2024-12-27 15:35:21 +0900

    typescriptのルール追加・改造

--- eslint.config.js
+++ eslint.config.js
@@ -14,8 +14,8 @@ const compat = new FlatCompat({
  */
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
   ...compat.config({
     extends: ['next/core-web-vitals'],
   }),
@@ -24,6 +24,32 @@ export default tseslint.config(
   {
     ...jsxA11y.flatConfigs.recommended,
     plugins: { 'jsx-a11y': jsxA11y },
+  },
+  {
+    rules: {
+      "semi": "off",
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "args": "all",
+          "argsIgnorePattern": "^_",
+          "caughtErrors": "all",
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "ignoreRestSiblings": true
+        },
+      ],
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
   },
   {
     files: ['*.stories.tsx'],
