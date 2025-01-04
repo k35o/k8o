144b3d0a072cd301452902e69d7a950c1840dd98
Koki Sakano <kosakanoki@gmail.com>
2025-01-04 12:22:58 +0900

    成功したらトーストを出す

--- src/app/_components/contact-to-me/contact-to-me.tsx
+++ src/app/_components/contact-to-me/contact-to-me.tsx
@@ -8,13 +8,10 @@ import { Textarea } from '@/components/form/textarea';
 import { FormControl } from '@/components/form/form-control';
 import { Button } from '@/components/button';
 import { contact } from '@/app/_actions/contact-to-me';
+import { useToast } from '@/components/toast';
 
 export const ContactToMe: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [state, formAction, pending] = useActionState(contact, {
-    success: true,
-    defaultValue: '',
-  });
 
   const onOpen = useCallback(() => {
     setIsOpen(true);
@@ -30,40 +27,66 @@ export const ContactToMe: FC = () => {
         <span className="sr-only">お問い合わせ</span>
         <Send className="size-8" />
       </IconButton>
-      {isOpen && (
-        <Modal title="お問い合わせ" onClose={onClose}>
-          <form className="flex flex-col gap-4" action={formAction}>
-            <FormControl
-              label="お問い合わせ内容"
-              isInvalid={!state.success}
-              errorText={state.success ? '' : state.message}
-              helpText="255文字以内で入力してください"
-              renderInput={({
-                id,
-                describedbyId,
-                isDisabled,
-                isInvalid,
-              }) => (
-                <Textarea
-                  id={id}
-                  name="message"
-                  defaultValue={state.defaultValue}
-                  describedbyId={describedbyId}
-                  isDisabled={isDisabled}
-                  isInvalid={isInvalid}
-                  isRequired={true}
-                  rows={5}
-                />
-              )}
-            />
-            <div className="w-full">
-              <Button type="submit" fullWidth disabled={pending}>
-                送信
-              </Button>
-            </div>
-          </form>
-        </Modal>
-      )}
+      {isOpen && <ContactToMeModal onClose={onClose} />}
     </>
+  );
+};
+
+const ContactToMeModal: FC<{ onClose: () => void }> = ({
+  onClose,
+}) => {
+  const [state, formAction, pending] = useActionState(contact, {
+    success: null,
+    defaultValue: '',
+  });
+  const { onOpen: onToastOpen } = useToast();
+  const [prevSuccess, setPrevSuccess] = useState<boolean | null>(
+    null,
+  );
+
+  // TODO: To locate the bad setState() call inside `ContactToMeModal`,
+  if (prevSuccess !== state.success) {
+    if (state.success) {
+      setPrevSuccess(state.success);
+      onToastOpen('success', 'お問い合わせの送信に成功しました');
+      onClose();
+    }
+  }
+
+  return (
+    <Modal title="お問い合わせ" onClose={onClose}>
+      <form className="flex flex-col gap-4" action={formAction}>
+        <FormControl
+          label="お問い合わせ内容"
+          isInvalid={state.success === false}
+          errorText={
+            state.success === false ? state.message : undefined
+          }
+          helpText="255文字以内で入力してください"
+          renderInput={({
+            id,
+            describedbyId,
+            isDisabled,
+            isInvalid,
+          }) => (
+            <Textarea
+              id={id}
+              name="message"
+              defaultValue={state.defaultValue}
+              describedbyId={describedbyId}
+              isDisabled={isDisabled}
+              isInvalid={isInvalid}
+              isRequired={true}
+              rows={5}
+            />
+          )}
+        />
+        <div className="w-full">
+          <Button type="submit" fullWidth disabled={pending}>
+            送信
+          </Button>
+        </div>
+      </form>
+    </Modal>
   );
 };
