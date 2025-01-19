fb6fa61058b4e432db6e4fe5a94aa3130a9a83f8
Koki Sakano <kosakanoki@gmail.com>
2025-01-19 21:38:58 +0900

    headingTreeを外に持っていく

--- src/app/blog/_components/table-of-context/index.tsx
+++ src/app/blog/_components/table-of-context/index.tsx
@@ -21,126 +21,129 @@ type HeadingTree = {
   }[];
 };
 
-export const TableOfContext: FC<{ slug: string }> = async ({
-  slug,
-}) => {
-  // Heading 2, 3, 4 のみをツリーに起こす
+const getHeadingTree = cache(async (slug: string) => {
   let headingTree: HeadingTree = {
     depth: 0,
     children: [],
   };
-  await cache(async (slug: string) =>
-    remark()
-      .use(function () {
-        return (tree: Root) => {
-          for (const content of tree.children) {
-            if (content.type !== 'heading') {
-              continue;
-            }
-            if (
-              content.children[0]?.type !== 'text' ||
-              content.depth > 4
-            ) {
-              continue;
-            }
-
-            if (content.depth === 2) {
-              headingTree.children.push({
-                depth: 1,
-                text: content.children[0].value,
-                children: [],
-              });
-              continue;
-            }
-            if (content.depth === 3) {
-              const depth1Length = headingTree.children.length;
-              const depth1RestTree = headingTree.children.slice(
-                0,
-                depth1Length - 1,
-              );
-              const depth1LastTree =
-                headingTree.children[depth1Length - 1];
-              if (!depth1LastTree) {
-                continue;
-              }
-              headingTree = {
-                ...headingTree,
-                children: [
-                  ...depth1RestTree,
-                  {
-                    ...depth1LastTree,
-                    children: [
-                      ...depth1LastTree.children,
-                      {
-                        depth: 2,
-                        text: content.children[0].value,
-                        children: [],
-                      },
-                    ],
-                  },
-                ],
-              };
-              continue;
-            }
-            if (content.depth === 4) {
-              const depth1Length = headingTree.children.length;
-              const depth1RestTree = headingTree.children.slice(
-                0,
-                depth1Length - 1,
-              );
-              const depth1LastTree =
-                headingTree.children[depth1Length - 1];
-
-              if (!depth1LastTree) {
-                continue;
-              }
-
-              const depth2Length = depth1LastTree.children.length;
-              const depth2RestTree = depth1LastTree.children.slice(
-                0,
-                depth2Length - 1,
-              );
-              const depth2LastTree =
-                depth1LastTree.children[depth2Length - 1];
-
-              if (!depth2LastTree) {
-                continue;
-              }
-
-              headingTree = {
-                ...headingTree,
-                children: [
-                  ...depth1RestTree,
-                  {
-                    ...depth1LastTree,
-                    children: [
-                      ...depth2RestTree,
-                      {
-                        ...depth2LastTree,
-                        children: [
-                          ...depth2LastTree.children,
-                          {
-                            depth: 3,
-                            text: content.children[0].value,
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              };
-              continue;
-            }
+  await remark()
+    .use(function () {
+      return (tree: Root) => {
+        for (const content of tree.children) {
+          if (content.type !== 'heading') {
+            continue;
           }
-        };
-      })
-      .process(
-        readFileSync(
-          process.cwd() + `/src/app/blog/${slug}/page.mdx`,
-          'utf-8',
-        ),
+          if (
+            content.children[0]?.type !== 'text' ||
+            content.depth > 4
+          ) {
+            continue;
+          }
+
+          if (content.depth === 2) {
+            headingTree.children.push({
+              depth: 1,
+              text: content.children[0].value,
+              children: [],
+            });
+            continue;
+          }
+          if (content.depth === 3) {
+            const depth1Length = headingTree.children.length;
+            const depth1RestTree = headingTree.children.slice(
+              0,
+              depth1Length - 1,
+            );
+            const depth1LastTree =
+              headingTree.children[depth1Length - 1];
+            if (!depth1LastTree) {
+              continue;
+            }
+            headingTree = {
+              ...headingTree,
+              children: [
+                ...depth1RestTree,
+                {
+                  ...depth1LastTree,
+                  children: [
+                    ...depth1LastTree.children,
+                    {
+                      depth: 2,
+                      text: content.children[0].value,
+                      children: [],
+                    },
+                  ],
+                },
+              ],
+            };
+            continue;
+          }
+          if (content.depth === 4) {
+            const depth1Length = headingTree.children.length;
+            const depth1RestTree = headingTree.children.slice(
+              0,
+              depth1Length - 1,
+            );
+            const depth1LastTree =
+              headingTree.children[depth1Length - 1];
+
+            if (!depth1LastTree) {
+              continue;
+            }
+
+            const depth2Length = depth1LastTree.children.length;
+            const depth2RestTree = depth1LastTree.children.slice(
+              0,
+              depth2Length - 1,
+            );
+            const depth2LastTree =
+              depth1LastTree.children[depth2Length - 1];
+
+            if (!depth2LastTree) {
+              continue;
+            }
+
+            headingTree = {
+              ...headingTree,
+              children: [
+                ...depth1RestTree,
+                {
+                  ...depth1LastTree,
+                  children: [
+                    ...depth2RestTree,
+                    {
+                      ...depth2LastTree,
+                      children: [
+                        ...depth2LastTree.children,
+                        {
+                          depth: 3,
+                          text: content.children[0].value,
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            };
+            continue;
+          }
+        }
+      };
+    })
+    .process(
+      readFileSync(
+        process.cwd() + `/src/app/blog/${slug}/page.mdx`,
+        'utf-8',
       ),
-  )(slug);
+    );
+  return headingTree;
+});
+
+export const TableOfContext: FC<{ slug: string }> = async ({
+  slug,
+}) => {
+  // Heading 2, 3, 4 のみをツリーに起こす
+  const headingTree = await getHeadingTree(slug);
 
   if (headingTree.children.length === 0) {
     return null;
