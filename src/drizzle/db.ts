eb7a17fdbbc53af78a568daf68cb50f11da09600
Koki Sakano <kosakanoki@gmail.com>
2025-01-19 12:51:11 +0900

    POSTGRES_URLに変更

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -3,5 +3,5 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 
-const sql = neon(process.env.DATABASE_URL ?? '');
+const sql = neon(process.env.POSTGRES_URL ?? '');
 export const db = drizzle({ client: sql, schema });
