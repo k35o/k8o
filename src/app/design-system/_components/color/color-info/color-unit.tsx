815d708073506161db56363766f69728895cbee2
Koki Sakano <kosakanoki@gmail.com>
2025-02-05 20:36:17 +0900

    contrastの表示と見た目の整理

---
+++ src/app/design-system/_components/color/color-info/color-unit.tsx
@@ -0,0 +1,107 @@
+import { toPrecision } from '@/utils/number/to-precision';
+import { calcContrast } from '@/utils/color/calc_contrast';
+import { FC } from 'react';
+import { cn } from '@/utils/cn';
+
+export const ColorUnit: FC<{
+  colorCode: string;
+  colorCodeDark: string;
+  contrastName: string;
+  contrastCode: string;
+  contrastCodeDark: string;
+  variant: 'background' | 'border' | 'foreground';
+}> = ({
+  colorCode,
+  colorCodeDark,
+  contrastName,
+  contrastCode,
+  contrastCodeDark,
+  variant,
+}) => {
+  const colorContrast = toPrecision(
+    calcContrast(colorCode, contrastCode),
+    2,
+  );
+  const colorContrastDark = toPrecision(
+    calcContrast(colorCodeDark, contrastCodeDark),
+    2,
+  );
+
+  return (
+    <div className="flex w-32 flex-col items-center gap-2">
+      <p>{contrastName}</p>
+      <div className="flex items-center gap-3">
+        <div className="flex flex-col items-center gap-1">
+          <div
+            className="border-border-base flex size-12 items-center justify-center rounded-full border"
+            style={
+              variant === 'foreground'
+                ? {
+                    color: colorCode,
+                    backgroundColor: contrastCode,
+                  }
+                : variant === 'background'
+                  ? {
+                      color: contrastCode,
+                      backgroundColor: colorCode,
+                    }
+                  : {
+                      borderColor: colorCode,
+                      backgroundColor: contrastCode,
+                    }
+            }
+          >
+            light
+          </div>
+          {variant !== 'border' && (
+            <p
+              className={cn(
+                'text-xs',
+                colorContrast >= 4.5
+                  ? 'text-fg-success'
+                  : 'text-fg-error',
+              )}
+            >
+              {colorContrast}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col items-center gap-1">
+          <div
+            className="border-border-base flex size-12 items-center justify-center rounded-full border"
+            style={
+              variant === 'foreground'
+                ? {
+                    color: colorCodeDark,
+                    backgroundColor: contrastCodeDark,
+                  }
+                : variant === 'background'
+                  ? {
+                      color: contrastCodeDark,
+                      backgroundColor: colorCodeDark,
+                    }
+                  : {
+                      borderColor: colorCodeDark,
+                      backgroundColor: contrastCodeDark,
+                    }
+            }
+          >
+            dark
+          </div>
+          {variant !== 'border' && (
+            <p
+              className={cn(
+                'text-xs',
+                colorContrastDark >= 4.5
+                  ? 'text-fg-success'
+                  : 'text-fg-error',
+              )}
+            >
+              {colorContrastDark}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
