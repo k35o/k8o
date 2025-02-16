eb7c5858a6fc9daf6404dff9062fc78938527959
Koki Sakano <kosakanoki@gmail.com>
2025-02-16 13:24:17 +0900

    localはlocalのデータベースを利用するように

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,7 +1,24 @@
 import '@/drizzle/env-config';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
+import { neon, neonConfig } from '@neondatabase/serverless';
 import * as schema from './schema';
+import { ws } from 'msw';
 
-const sql = neon(process.env.POSTGRES_URL ?? '');
+const DATABASE_URL = process.env.POSTGRES_URL ?? '';
+
+if (process.env.NODE_ENV === 'development') {
+  neonConfig.fetchEndpoint = (host) => {
+    const [protocol, port] =
+      host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
+    return `${protocol}://${host}:${port.toString()}/sql`;
+  };
+  const connectionStringUrl = new URL(DATABASE_URL);
+  neonConfig.useSecureWebSocket =
+    connectionStringUrl.hostname !== 'db.localtest.me';
+  neonConfig.wsProxy = (host) =>
+    host === 'db.localtest.me' ? `${host}:4444/v2` : `${host}/v2`;
+}
+neonConfig.webSocketConstructor = ws;
+
+const sql = neon(DATABASE_URL);
 export const db = drizzle({ client: sql, schema });
