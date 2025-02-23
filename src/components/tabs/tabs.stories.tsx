a53cbea52ce93c5207be54692262c29c04b86c50
Koki Sakano <kosakanoki@gmail.com>
2025-02-23 17:47:49 +0900

    tabsの修正

--- src/components/tabs/tabs.stories.tsx
+++ src/components/tabs/tabs.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Tabs } from './tabs';
 import { expect, userEvent, waitFor, within } from '@storybook/test';
+import { getRouter } from '@storybook/nextjs/navigation.mock';
+import { Alert } from '../alert';
 
 const meta: Meta<typeof Tabs.Root> = {
   title: 'components/tabs',
@@ -25,7 +27,9 @@ export const Primary: Story = {
   ),
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    await expect(canvas.getByRole('tabpanel')).toHaveTextContent('Panel1');
+    await expect(canvas.getByRole('tabpanel')).toHaveTextContent(
+      'Panel1',
+    );
 
     await userEvent.keyboard('{arrowright}');
     await waitFor(() =>
@@ -61,20 +65,32 @@ export const DefaultSelected: Story = {
   ),
 };
 
-export const Link: Story = {
+export const HashLink: Story = {
+  beforeEach() {
+    getRouter().push.mockImplementation((hash: string) => {
+      window.location.hash = hash;
+    });
+  },
   render: () => (
-    <Tabs.Root
-      ids={['tab1', 'tab2', 'tab3']}
-      defaultSelectedId="tab2"
-    >
-      <Tabs.List label="Tabs Example">
-        <Tabs.LinkTab id="tab1">Tab1</Tabs.LinkTab>
-        <Tabs.LinkTab id="tab2">Tab2</Tabs.LinkTab>
-        <Tabs.LinkTab id="tab3">Tab3</Tabs.LinkTab>
-      </Tabs.List>
-      <Tabs.Panel id="tab1">Panel1</Tabs.Panel>
-      <Tabs.Panel id="tab2">Panel2</Tabs.Panel>
-      <Tabs.Panel id="tab3">Panel3</Tabs.Panel>
-    </Tabs.Root>
+    <div className="flex flex-col gap-4">
+      <Alert
+        status="warning"
+        message="Storybook上でクリックを使ってタブを切り替えた場合は別のWindowに遷移します"
+      />
+      <Tabs.Root
+        ids={['tab1', 'tab2', 'tab3']}
+        defaultSelectedId="tab2"
+        hashLink
+      >
+        <Tabs.List label="Tabs Example">
+          <Tabs.Tab id="tab1">Tab1</Tabs.Tab>
+          <Tabs.Tab id="tab2">Tab2</Tabs.Tab>
+          <Tabs.Tab id="tab3">Tab3</Tabs.Tab>
+        </Tabs.List>
+        <Tabs.Panel id="tab1">Panel1</Tabs.Panel>
+        <Tabs.Panel id="tab2">Panel2</Tabs.Panel>
+        <Tabs.Panel id="tab3">Panel3</Tabs.Panel>
+      </Tabs.Root>
+    </div>
   ),
 };
--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import {
   createContext,
   FC,
+  KeyboardEvent,
   PropsWithChildren,
   use,
   useEffect,
@@ -13,13 +14,15 @@ import {
 } from 'react';
 import * as motion from 'motion/react-client';
 import { cn } from '@/utils/cn';
+import { useRouter } from 'next/navigation';
+import { useHash } from '@/hooks/hash';
 
 type TabsContext = {
   rootId: string;
   ids: [string, ...string[]];
-  activeIndex: number;
-  selectedId: string | null;
+  selectedId: string;
   setSelectedId: (id: string) => void;
+  hashLink: boolean;
 };
 
 const TabsProvider = createContext<TabsContext | undefined>(
@@ -38,27 +41,42 @@ const useTabsState = (): TabsContext => {
 
 const Root: FC<
   PropsWithChildren<{
+    hashLink?: boolean;
     defaultSelectedId?: string | null;
     ids: [string, ...string[]];
   }>
-> = ({ defaultSelectedId = null, ids, children }) => {
+> = ({
+  defaultSelectedId = null,
+  ids,
+  children,
+  hashLink = false,
+}) => {
   const defaultIndex = defaultSelectedId
     ? ids.indexOf(defaultSelectedId)
     : 0;
   const [selectedId, setSelectedId] = useState<string>(
     defaultSelectedId ?? ids[defaultIndex] ?? ids[0],
   );
-  const activeIndex = ids.indexOf(selectedId);
   const rootId = useId();
+  const router = useRouter();
+  const hash = useHash();
+
+  const hansleChangeHash = (id: string) => {
+    router.push(`#${id}`);
+  };
 
   return (
     <TabsProvider
       value={{
         rootId,
         ids,
-        activeIndex,
-        selectedId,
-        setSelectedId,
+        selectedId: hashLink
+          ? hash === '' || hash === null
+            ? ids[0]
+            : hash
+          : selectedId,
+        setSelectedId: hashLink ? hansleChangeHash : setSelectedId,
+        hashLink,
       }}
     >
       <div className="flex flex-col gap-1">{children}</div>
@@ -89,9 +107,10 @@ const Tab: FC<PropsWithChildren<{ id: string }>> = ({
   id,
   children,
 }) => {
-  const { rootId, ids, activeIndex, selectedId, setSelectedId } =
+  const { rootId, hashLink, ids, selectedId, setSelectedId } =
     useTabsState();
-  const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLAnchorElement & HTMLDivElement>(null);
+  const activeIndex = ids.indexOf(selectedId);
   const index = ids.indexOf(id);
 
   useEffect(() => {
@@ -100,113 +119,55 @@ const Tab: FC<PropsWithChildren<{ id: string }>> = ({
     }
   }, [activeIndex, index]);
 
-  return (
+  const props = {
+    id: `${rootId}-tab-${id}`,
+    role: 'tab',
+    'aria-controls':
+      selectedId === id ? `${rootId}-panel-${id}` : undefined,
+    'aria-selected': selectedId === id,
+    tabIndex: activeIndex === index ? 0 : -1,
+    className: cn(
+      'relative cursor-pointer rounded-md p-3',
+      'focus-visible:bordertransparent focus-visible:ring-border-info focus-visible:ring-2 focus-visible:outline-hidden',
+    ),
+    onKeyDown: (e: KeyboardEvent) => {
+      if (e.key === 'ArrowLeft') {
+        const nextActiveIndex =
+          index === 0 ? ids.length - 1 : index - 1;
+        setSelectedId(ids[nextActiveIndex] ?? ids[0]);
+        return;
+      }
+      if (e.key === 'ArrowRight') {
+        const nextActiveIndex =
+          index === ids.length - 1 ? 0 : index + 1;
+        setSelectedId(ids[nextActiveIndex] ?? ids[0]);
+        return;
+      }
+    },
+    children: (
+      <>
+        {selectedId === id && (
+          <motion.div
+            layoutId="underline"
+            className="bg-primary-border absolute right-0 -bottom-0.5 left-0 h-1"
+          />
+        )}
+        {children}
+      </>
+    ),
+  };
+
+  return hashLink ? (
+    <Link ref={ref} href={`#${id}`} {...props} />
+  ) : (
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events -- propsを別の場所で定義することで解決済み
     <div
       ref={ref}
-      id={`${rootId}-tab-${id}`}
-      role="tab"
-      aria-controls={
-        selectedId === id ? `${rootId}-panel-${id}` : undefined
-      }
-      aria-selected={selectedId === id}
-      tabIndex={activeIndex === index ? 0 : -1}
-      className={cn(
-        'relative cursor-pointer rounded-md p-3',
-        'focus-visible:bordertransparent focus-visible:ring-border-info focus-visible:ring-2 focus-visible:outline-hidden',
-      )}
-      onKeyDown={(e) => {
-        if (e.key === 'ArrowLeft') {
-          const nextActiveIndex =
-            index === 0 ? ids.length - 1 : index - 1;
-          setSelectedId(ids[nextActiveIndex] ?? ids[0]);
-          return;
-        }
-        if (e.key === 'ArrowRight') {
-          const nextActiveIndex =
-            index === ids.length - 1 ? 0 : index + 1;
-          setSelectedId(ids[nextActiveIndex] ?? ids[0]);
-          return;
-        }
-        if (e.key === 'Enter') {
-          setSelectedId(id);
-          return;
-        }
-      }}
       onClick={() => {
         setSelectedId(id);
       }}
-    >
-      {selectedId === id && (
-        <motion.div
-          layoutId="underline"
-          className="bg-primary-border absolute right-0 -bottom-0.5 left-0 h-1"
-        />
-      )}
-      {children}
-    </div>
-  );
-};
-
-const LinkTab: FC<PropsWithChildren<{ id: string }>> = ({
-  id,
-  children,
-}) => {
-  const { rootId, ids, activeIndex, selectedId, setSelectedId } =
-    useTabsState();
-  const ref = useRef<HTMLAnchorElement>(null);
-  const index = ids.indexOf(id);
-
-  useEffect(() => {
-    if (activeIndex === index) {
-      ref.current?.focus();
-    }
-  }, [activeIndex, index]);
-
-  return (
-    <Link
-      ref={ref}
-      href={`#${id}`}
-      id={`${rootId}-tab-${id}`}
-      role="tab"
-      aria-controls={
-        selectedId === id ? `${rootId}-panel-${id}` : undefined
-      }
-      aria-selected={selectedId === id}
-      tabIndex={activeIndex === index ? 0 : -1}
-      className={cn(
-        'relative cursor-pointer rounded-md p-3',
-        'focus-visible:bordertransparent focus-visible:ring-border-info focus-visible:ring-2 focus-visible:outline-hidden',
-      )}
-      onKeyDown={(e) => {
-        if (e.key === 'ArrowLeft') {
-          const nextActiveIndex =
-            index === 0 ? ids.length - 1 : index - 1;
-          setSelectedId(ids[nextActiveIndex] ?? ids[0]);
-          return;
-        }
-        if (e.key === 'ArrowRight') {
-          const nextActiveIndex =
-            index === ids.length - 1 ? 0 : index + 1;
-          setSelectedId(ids[nextActiveIndex] ?? ids[0]);
-          return;
-        }
-        if (e.key === 'Enter') {
-          setSelectedId(id);
-          return;
-        }
-      }}
-      onClick={() => {
-        setSelectedId(id);
-      }}
-    >
-      {selectedId === id && (
-        <motion.div
-          layoutId="underline"
-          className="bg-primary-border absolute right-0 -bottom-0.5 left-0 h-1"
-        />
-      )}
-      {children}
-    </Link>
+      {...props}
+    />
   );
 };
 
@@ -240,6 +201,5 @@ export const Tabs = {
   Root,
   List,
   Tab,
-  LinkTab,
   Panel,
 };
