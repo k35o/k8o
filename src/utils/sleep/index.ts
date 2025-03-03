e6f4c456df84684aea6489bbd0420a446e2f6209
k8o <kosakanoki@gmail.com>
2025-03-03 10:09:07 +0900

    fmt

--- src/utils/sleep/index.ts
+++ src/utils/sleep/index.ts
@@ -1,3 +1,5 @@
-export const sleep = (ms: number): Promise<void> => {
-	return new Promise((resolve) => setTimeout(resolve, ms));
+export const sleep = (
+  ms: number,
+): Promise<void> => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 };
