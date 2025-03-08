7201cd6ad17accd792f0b20aaf8a899f83ce936f
Koki Sakano <kosakanoki@gmail.com>
2025-03-08 20:10:56 +0900

    iconの追加

--- src/components/icons/arte-odyssey.tsx
+++ src/components/icons/arte-odyssey.tsx
@@ -1,4 +1,18 @@
-import { Earth } from 'lucide-react';
+import { BaseIcon, BaseIconProps } from './base';
+import arteodyssey from '@/app/_images/arteodyssey.png';
+import Image from 'next/image';
+import { FC } from 'react';
 
 // 仮のアイコン
-export const ArteOdyssey = Earth;
+export const ArteOdyssey: FC<Partial<BaseIconProps>> = ({
+  size = 'md',
+}) => {
+  return (
+    <BaseIcon
+      size={size}
+      renderItem={(props) => (
+        <Image {...props} src={arteodyssey} alt="" />
+      )}
+    />
+  );
+};
