26578571112ea9ddbf69489b8f0cf22475e6c6f8
Koki Sakano <kosakanoki@gmail.com>
2025-04-11 10:02:31 +0900

    descriptionを構造的にする

--- src/app/contrast-checker/_components/description/description.tsx
+++ src/app/contrast-checker/_components/description/description.tsx
@@ -1,18 +1,34 @@
+import { Anchor } from '@/components/anchor';
 import { FC } from 'react';
 
 export const Description: FC = () => {
   return (
     <div className="border-border-emphasize w-full rounded-md border-2 p-4">
-      <p>
-        WCAG
-        2.1によると、AA基準における大文字のテキストの最小コントラスト比は4.5:1、小文字のテキストの最小コントラスト比は3:1です。
-      </p>
-      <p>
-        AAA基準においては、大文字のテキストの最小コントラスト比は7:1、小文字のテキストの最小コントラスト比は4.5:1です。
-      </p>
-      <p className="mt-2">
-        大文字のテキストは、18pt（24px）以上、または太字の場合は14pt（18.66px）以上である場合に適用されます。小文字のテキストは、14pt（18.66px）以上、または太字の場合は12pt（16px）以上である場合に適用されます。
-      </p>
+      <div className="flex flex-col gap-3">
+        <p className="text-xl font-bold">
+          <Anchor href="https://www.w3.org/TR/WCAG22/">
+            WCAG 2.2
+          </Anchor>
+          によるコントラスト比の基準
+        </p>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1">
+            <p className="text-lg font-bold">AA基準</p>
+            <p>
+              文字のコントラスト比が最低でも4.5:1、大文字の場合は3:1
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <p className="text-lg font-bold">AAA基準</p>
+            <p>
+              文字のコントラスト比が最低でも7:1、大文字の場合は4.5:1
+            </p>
+          </div>
+          <p className="text-fg-mute self-end text-sm">
+            大文字とは18pt（24px）以上、太字の場合は14pt（18.66px）以上を指します。
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
