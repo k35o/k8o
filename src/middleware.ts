1c3c0a291b249883bf697fbe8fd88a9f69fcd67e
Koki Sakano <kosakanoki@gmail.com>
2025-04-17 17:14:58 +0900

    slugがfeedの時は送信しない

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,7 +20,7 @@ export function middleware(request: NextRequest) {
   // blog
   if (request.nextUrl.pathname.startsWith('/blog')) {
     const slug = request.nextUrl.pathname.split('/')[2];
-    if (slug) {
+    if (slug && slug !== 'feed') {
       after(() => {
         void fetch(`${request.nextUrl.origin}/api/blog/views`, {
           method: 'POST',
