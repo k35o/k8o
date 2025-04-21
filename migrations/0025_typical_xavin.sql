d55a62f4b58843141aa468813201cd59fade6e3c
k35o <kouki_sakano@dwango.co.jp>
2025-04-21 20:50:01 +0900

    migrationファイルの作成

---
+++ migrations/0025_typical_xavin.sql
@@ -0,0 +1,16 @@
+INSERT INTO tags (id, name) VALUES (28, 'Iterator Helpers');
+INSERT INTO tags (id, name) VALUES (29, 'CSS Gradients');
+INSERT INTO tags (id, name) VALUES (30, 'CSS');
+
+INSERT INTO blogs (id, slug) VALUES (13, 'iterator-methods');
+INSERT INTO blog_views (blog_id, views) VALUES (13, 0);
+INSERT INTO blog_tag (blog_id, tag_id) VALUES (13, 10);
+INSERT INTO blog_tag (blog_id, tag_id) VALUES (13, 11);
+INSERT INTO blog_tag (blog_id, tag_id) VALUES (13, 28);
+
+INSERT INTO blogs (id, slug) VALUES (14, 'single-color-gradients');
+INSERT INTO blog_views (blog_id, views) VALUES (14, 0);
+INSERT INTO blog_tag (blog_id, tag_id) VALUES (14, 8);
+INSERT INTO blog_tag (blog_id, tag_id) VALUES (14, 11);
+INSERT INTO blog_tag (blog_id, tag_id) VALUES (14, 29);
+INSERT INTO blog_tag (blog_id, tag_id) VALUES (14, 30);
