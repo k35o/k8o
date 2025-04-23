951045e9664221ba03d46e5f289aa72501eef72c
k8o <kosakanoki@gmail.com>
2025-04-23 22:54:10 +0900

    talks

--- src/app/talks/opengraph-image.tsx
+++ src/app/talks/opengraph-image.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from 'next/og';
 
 export const runtime = 'edge';
 
-export const alt = 'c';
+export const alt = 'talks';
 export const size = {
   width: 1200,
   height: 600,
