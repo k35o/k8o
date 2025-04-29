19bb4186047a5c1c57499bfb090c8a901aff1cb6
Koki Sakano <kosakanoki@gmail.com>
2025-04-29 23:53:40 +0900

    consoleの削除

--- src/mocks/db.mock.ts
+++ src/mocks/db.mock.ts
@@ -1,7 +1,6 @@
 import { relations, schema } from '../database/schema';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-console.log('Mocking database connection');
 export const db = drizzle.mock({
   schema: {
     ...schema,
