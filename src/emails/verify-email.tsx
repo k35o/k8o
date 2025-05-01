0478394ca2f25f8581397ff6a83255f4879a16ec
Koki Sakano <kosakanoki@gmail.com>
2025-05-01 10:52:06 +0900

    メール文面の作成

--- src/emails/verify-email.tsx
+++ src/emails/verify-email.tsx
@@ -17,8 +17,6 @@ type Props = {
   expiresAt: Date;
 };
 
-// TODO: 適当なのでちゃんと組む
-// 間違えて送っちゃた時のことなど
 const VerifyEmail = ({ email, token, expiresAt }: Props) => {
   return (
     <Html>
@@ -34,23 +32,33 @@ const VerifyEmail = ({ email, token, expiresAt }: Props) => {
         >
           <Container className="mx-auto my-[40px] rounded-xl bg-white text-[#18181b]">
             <Heading className="text-fg-base text-center text-2xl font-bold">
-              k8o.meへのメールアドレス確認
+              k8o.meを購読いただきありがとうございます
             </Heading>
-            <div>
-              <Text className="m-0 text-center text-base font-normal">
-                {formatDate(expiresAt, 'yyyy年M月d日(E) HH時mm分')}
-                までに
-              </Text>
-              <Text className="m-0 text-center text-base font-normal">
-                下記のリンクをクリックしてください
-              </Text>
-            </div>
+            <Text className="m-0 text-center text-base font-normal">
+              メールアドレスの確認を行うため、下記のリンクをクリックしてください
+            </Text>
             <Link
-              href={`https://k8o.me/api/verify?email=${email}&token=${token}`}
-              className="m-4 block rounded-lg bg-blue-500 px-4 py-2 text-center text-white hover:bg-blue-600"
+              href={`https://k8o.me/api/subscriptions/verify?email=${email}&token=${token}`}
+              className="m-4 mb-1 block rounded-lg bg-blue-500 px-4 py-2 text-center text-white hover:bg-blue-600"
             >
               メールアドレスを確認する
             </Link>
+            <Text className="mx-4 my-0 text-sm text-[#3f3f46]">
+              ※このリンクの有効期限は
+              {formatDate(expiresAt, 'yyyy年M月d日(E) HH時mm分')}
+              です。
+            </Text>
+            <Text className="mx-4 my-0 text-sm text-[#3f3f46]">
+              ※期限を過ぎた場合は、お手数ですが再度ご登録をお願いいたします。
+            </Text>
+            <Text className="text-center">
+              このメールにお心当たりがない場合は、誤って送信された可能性があります。
+              <br />
+              その際はお手数ですが、このメールを破棄してください。
+            </Text>
+            <Text className="text-center">
+              ご不明点がありましたら、このメールに返信する形でお問い合わせください
+            </Text>
           </Container>
         </Body>
       </Tailwind>
