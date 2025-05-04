2331f1e0b3f6002f6260e51a5795f455f49c6fc2
k8o <kosakanoki@gmail.com>
2025-05-04 17:13:06 +0900

    @types/reactも追加

--- migrations/0029_stiff_talon.sql
+++ migrations/0029_stiff_talon.sql
@@ -1,5 +1,6 @@
 INSERT INTO tags (id, name) VALUES (31, 'TSKaigi');
 INSERT INTO tags (id, name) VALUES (32, 'useReducer');
+INSERT INTO tags (id, name) VALUES (33, '@types/react');
 
 INSERT INTO blogs (id, slug, published) VALUES (15, 'react19-usereducer-ts-type-inference', false);
 INSERT INTO blog_views (blog_id, views) VALUES (15, 0);
@@ -7,3 +8,4 @@ INSERT INTO blog_tag (blog_id, tag_id) VALUES (15, 1);
 INSERT INTO blog_tag (blog_id, tag_id) VALUES (15, 2);
 INSERT INTO blog_tag (blog_id, tag_id) VALUES (15, 31);
 INSERT INTO blog_tag (blog_id, tag_id) VALUES (15, 32);
+INSERT INTO blog_tag (blog_id, tag_id) VALUES (15, 33);
