9ca3a80c5f1fef243941a76621288e0e6a2baa56
k8o <kosakanoki@gmail.com>
2025-05-11 19:10:08 +0900

    VERCEL_ENVを見る

--- src/app/blog/(articles)/react19-usereducer-ts-type-inference/layout.tsx
+++ src/app/blog/(articles)/react19-usereducer-ts-type-inference/layout.tsx
@@ -5,7 +5,7 @@ import { PropsWithChildren } from 'react';
 const slug = 'react19-usereducer-ts-type-inference';
 
 export default function Layout({ children }: PropsWithChildren) {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.VERCEL_ENV === 'production') {
     notFound();
   }
   return <BlogLayout slug={slug}>{children}</BlogLayout>;
