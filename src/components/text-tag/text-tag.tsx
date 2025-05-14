05ba4a8d599cfe60369ed20bf84d8d6808367621
k35o <kouki_sakano@dwango.co.jp>
2025-05-14 20:55:31 +0900

    text-tagをlinkにできるように colorも不要なので削除

--- src/components/text-tag/text-tag.tsx
+++ src/components/text-tag/text-tag.tsx
@@ -1,20 +1,36 @@
 import { cn } from '@/utils/cn';
+import { Route } from 'next';
+import Link from 'next/link';
 
 export const TextTag = ({
   text,
-  color = 'base',
   size = 'md',
+  href,
 }: {
   text: string;
-  color?: 'base' | 'white';
   size?: 'sm' | 'md';
+  href?: Route;
 }) => {
+  if (href) {
+    return (
+      <Link href={href}>
+        <span
+          className={cn(
+            'bg-bg-mute hover:bg-bg-emphasize inline-block rounded-full font-medium',
+            size === 'sm' && 'px-2 py-0.5 text-xs',
+            size === 'md' && 'px-3 py-1 text-sm',
+          )}
+        >
+          {text}
+        </span>
+      </Link>
+    );
+  }
+
   return (
     <span
       className={cn(
-        'inline-block rounded-full font-medium',
-        color === 'base' && 'bg-bg-mute',
-        color === 'white' && 'bg-bg-base',
+        'bg-bg-mute inline-block rounded-full font-medium',
         size === 'sm' && 'px-2 py-0.5 text-xs',
         size === 'md' && 'px-3 py-1 text-sm',
       )}
