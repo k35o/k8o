35c06649fad084792dd505a4d3082db0c3fb177b
k35o <kouki_sakano@dwango.co.jp>
2025-05-16 11:50:08 +0900

    vitest.setup.tsの改善

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,7 +1,4 @@
-import { beforeAll } from 'vitest';
 import { setProjectAnnotations } from '@storybook/nextjs';
 import * as projectAnnotations from './preview';
 
-const project = setProjectAnnotations([projectAnnotations]);
-
-beforeAll(project.beforeAll);
+setProjectAnnotations([projectAnnotations]);
