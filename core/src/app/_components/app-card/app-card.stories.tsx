3e1dd670620609c1d79e51b7c1a620d2c9df7d9f
k8o <kosakanoki@gmail.com>
2025-07-18 18:57:54 +0900

    remove lucide

--- core/src/app/_components/app-card/app-card.stories.tsx
+++ core/src/app/_components/app-card/app-card.stories.tsx
@@ -1,6 +1,6 @@
 import { AppCard } from './app-card';
+import { LinkIcon } from '@k8o/components/icons';
 import type { Meta, StoryObj } from '@storybook/nextjs';
-import { Link } from 'lucide-react';
 
 const meta: Meta<typeof AppCard> = {
   title: 'app/globals/app-card',
@@ -23,7 +23,7 @@ export const Primary: Story = {
 export const EmotionIsElement: Story = {
   args: {
     link: '/moji-count',
-    symbol: <Link className="text-primary-fg size-12" />,
+    symbol: <LinkIcon size="lg" />,
     title: 'もじカウント',
     description:
       'テキストの文字数を数えます。ひらがな・カタカナ・漢字・アルファベット・記号・絵文字など、文字の種類を問わず数えられます。',
