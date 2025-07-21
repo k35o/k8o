4b2e58109dfc45c95728fdff1450bece04918edf
k8o <kosakanoki@gmail.com>
2025-07-21 23:43:35 +0900

    blogsもmockを使うように

--- core/src/mocks/helpers/ratelimit.mock.ts
+++ core/src/mocks/helpers/ratelimit.mock.ts
@@ -1,5 +1,12 @@
 import { fn } from 'storybook/test';
 
+export enum RateLimitType {
+  GENERAL = 'general',
+  FEEDBACK = 'feedback',
+}
+
+export const checkRateLimit = fn();
+
 export const ratelimit = {
   limit: fn(),
 };
