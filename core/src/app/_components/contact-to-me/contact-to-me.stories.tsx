612922bb9eec7d352aea47d21c301dcb6ed0061b
k8o <kosakanoki@gmail.com>
2025-07-21 23:44:59 +0900

    openを正しい挙動に

--- core/src/app/_components/contact-to-me/contact-to-me.stories.tsx
+++ core/src/app/_components/contact-to-me/contact-to-me.stories.tsx
@@ -1,5 +1,6 @@
 import { ContactToMe } from './contact-to-me';
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { within } from 'storybook/test';
 
 const meta: Meta<typeof ContactToMe> = {
   title: 'app/globals/contact-to-me',
@@ -11,4 +12,12 @@ type Story = StoryObj<typeof ContactToMe>;
 
 export const Primary: Story = {};
 
-export const Open: Story = {};
+export const Open: Story = {
+  play: ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button', {
+      name: 'お問い合わせ',
+    });
+    button.click();
+  },
+};
