601d4c1bdaef4e27d9f88a0e2e0aba518a1cf610
k8o <kosakanoki@gmail.com>
2025-09-10 14:28:33 +0900

    @k8o/arte-odysseyのバージョンアップ

--- core/src/app/_providers/app.tsx
+++ core/src/app/_providers/app.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ComponentProvider } from '@k8o/arte-odyssey/providers';
+import { ArteOdysseyProvider } from '@k8o/arte-odyssey/providers';
 import { ThemeProvider } from 'next-themes';
 import type { FC, PropsWithChildren } from 'react';
 import { ColorFilters } from '@/app/_components/color-filters';
@@ -9,9 +9,9 @@ import '@/libs/zod';
 export const AppProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
     <ThemeProvider attribute="class">
-      <ComponentProvider>
+      <ArteOdysseyProvider>
         <ColorFilters.Provider>{children}</ColorFilters.Provider>
-      </ComponentProvider>
+      </ArteOdysseyProvider>
     </ThemeProvider>
   );
 };
