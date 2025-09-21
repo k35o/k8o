f0c5a25ffc93ce232159fb6656cb452440976c0a
k8o <kosakanoki@gmail.com>
2025-09-21 16:51:59 +0900

    link-rel-dns-prefetchの記事を作成

--- core/src/app/blog/(articles)/link-rel-dns-prefetch/opengraph-image.tsx
+++ core/src/app/blog/(articles)/link-rel-dns-prefetch/opengraph-image.tsx
@@ -1,7 +1,7 @@
 import { getBlogContent } from '#api/blog';
 import { OgImage } from '@/app/_components/og-image';
 
-export const alt = '';
+export const alt = 'リソースへのリクエスト前にドメイン名を解決するdns-prefetch';
 export const size = {
   width: 1200,
   height: 630,
