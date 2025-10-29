6940e3170aa2a70474c281331287414234897d9f
k8o <kosakanoki@gmail.com>
2025-10-29 14:20:36 +0900

    middlewareをproxyに

--- core/src/proxy.ts
+++ core/src/proxy.ts
@@ -1,6 +1,6 @@
 import { after, type NextRequest, NextResponse } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // blog
   if (request.nextUrl.pathname.startsWith('/blog')) {
     const slug = request.nextUrl.pathname.split('/')[2];
