dd45a8dbd46ad885c07fa5e421d2d22de5f5f244
k8o <kosakanoki@gmail.com>
2025-11-09 14:55:22 +0900

    Storyの修正

--- core/src/app/_components/playgrounds/input-file-webkitdirectory/webkit-relative-path-demo.stories.tsx
+++ core/src/app/_components/playgrounds/input-file-webkitdirectory/webkit-relative-path-demo.stories.tsx
@@ -1,10 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { Playground } from '../playground';
 import { WebkitRelativePathDemo } from './webkit-relative-path-demo';
 
 const meta = {
   title: 'playgrounds/input-file-webkitdirectory/WebkitRelativePathDemo',
   component: WebkitRelativePathDemo,
-  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <Playground>
+        <Story />
+      </Playground>
+    ),
+  ],
 } satisfies Meta<typeof WebkitRelativePathDemo>;
 
 export default meta;
